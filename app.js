0fe5af98de38caf9fe1685e2bd8c8e38537028e6
mwafy99 <imwafy99@gmail.com>
2020-03-07 17:30:50 +0200

    javascript file updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let lang = ['Hello', 'Bonjour', 'Hallo', 'Hola', 'Salam', 'Salve'];
 
 function hola() {
-    let h1 = document.querySelector('#hola').innerHTML = lang[Math.floor(Math.random() * lang.length)] + ' Ducky';
+    let h1 = document.querySelector('#hola').innerHTML = lang[Math.floor(Math.random() * lang.length)];
 }
 
 // Page Counter
@@ -11,8 +11,34 @@ let counter = 0;
 function count() {
     document.querySelector('.badge').innerHTML = "" + counter;
     counter++;
-    setTimeout(count, 1000);
+    // setTimeout(count, 1000);
+    window.setInterval(count,1000)
 }
 
 document.addEventListener("DOMContentLoaded", count);
 
+// Change colors
+document.querySelector('#color').onclick = function() {
+    let colors = ['pink','hotpink','dodgerblue','purple','red','green','darkgray','lightgreen'];
+    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+};
+
+// blinking
+function blink() {
+    let item = document.querySelector('#hola');
+    let thing = document.querySelector('#thing');
+    if (item.style.visibility === 'hidden' && thing.style.visibility === 'visible') {
+        item.style.visibility = 'visible';
+        thing.style.visibility = 'hidden';
+    } else {
+        item.style.visibility = 'hidden';
+        thing.style.visibility = 'visible';
+    }
+}
+window.setInterval(blink,900);
+
+// location
+navigator.geolocation.getCurrentPosition(function(p) {
+
+    document.querySelector('#location').innerHTML = 'I got your location! <br>' + p.coords.latitude +' , ' + p.coords.longitude;
+});
